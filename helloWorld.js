5ae4d82f46ed33bb24f685aef38bd09d3fe85804
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2023-06-26 16:03:27 +0800

    Create helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+// how to run a file inside the termina; ---> node <filename>
+console.log("Hello world");
+
+// addition of two numbers
+const add = (a, b) => {
+    return a + b;
+};
+
+const sum = add(1, 2);
+console.log(sum);
